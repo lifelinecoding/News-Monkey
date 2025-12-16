f2fd61f317acc0fea213ebc05084f2aca4ea20b1
Aditya patel <apatel89329@gmail.com>
2025-12-16 16:09:32 +0530

    Added few components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
 import './App.css';
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
 
+ export class App extends Component {
+  render() {
+    return (
+      <>
+      <Navbar title="NewsMonkey"/>
+      </>
+    )
+  }
+}
 
+export default App;
